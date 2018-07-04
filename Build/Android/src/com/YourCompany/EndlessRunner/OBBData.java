b6039b5c3c6f868b4387d41d4eb4a927ed2e5e57
Far <40501088+biggestbarnacle@users.noreply.github.com>
2018-07-03 21:34:44 -0600

    Poot
    Poot

--- Build/Android/src/com/YourCompany/EndlessRunner/OBBData.java
+++ Build/Android/src/com/YourCompany/EndlessRunner/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-980699451L // the length of the file in bytes
+1010994921L // the length of the file in bytes
 )
 };
 };
